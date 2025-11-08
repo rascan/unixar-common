a804af9c20faa865d1cc1b6538eb52fd7d4821ca
franciskisiara <18147985+franciskisiara@users.noreply.github.com>
2025-11-08 18:45:23 +0000

    Fix styling

--- src/Libs/helpers.php
+++ src/Libs/helpers.php
@@ -5,8 +5,10 @@ use Unixar\Models\Profile;
 use Unixar\Models\User;
 
 if (function_exists('profile')) {
-    function profile(?string $id = null): int | Profile {
+    function profile(?string $id = null): int|Profile
+    {
         $user = User::with(['profile'])->findOrFail(Auth::id());
+
         return $id ? $user->profile_id : $user->profile;
     }
 }
